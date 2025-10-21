416680fbe793675585494f732741921157a25aad
Jared Turck <jaredturck8@gmail.com>
2025-10-21 22:42:14 +0100

    added calculator for no. tokens

---
+++ datasets/parameter_calc.py
@@ -0,0 +1,26 @@
+d_model = 512
+nhead = d_model // 64
+dim_feedforward = d_model * 4
+no_transformer_layers = 6
+embedding_size = 2000
+max_length = 512
+batch_size = 256
+
+def transformer_param_count():
+
+    total = (embedding_size * d_model) + (max_length * d_model) + (no_transformer_layers *
+        (
+            (
+                (3 * d_model * d_model) + \
+                (3 * d_model) + \
+                (d_model * d_model) + d_model
+            ) + (
+                (d_model * dim_feedforward + dim_feedforward) + \
+                (dim_feedforward * d_model + d_model)
+            ) + (4 * d_model)
+        )
+    ) + embedding_size
+
+    print(f'{total:,} parameters, approximate training tokens {(total * 20) / 1_000_000:,.2f} million')
+
+transformer_param_count()
